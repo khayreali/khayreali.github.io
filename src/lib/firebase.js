dcdfd0b8f8e2fbded6bf7299e75b421150901cbc
khayreali <khayreahmed@gmail.com>
2024-12-01 23:03:51 -0500

    update firebase secrets

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -8,14 +8,15 @@ import { getFirestore } from 'firebase/firestore';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA5D7xGNIn4xQU-GfwUXlV5rjKu_f27DdA",
-  authDomain: "personal-website-98d70.firebaseapp.com",
-  projectId: "personal-website-98d70",
-  storageBucket: "personal-website-98d70.firebasestorage.app",
-  messagingSenderId: "991895801460",
-  appId: "1:991895801460:web:cf2f8d722d8b1f039ecb21",
-  measurementId: "G-Z0ZMG4JYCC"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
@@ -25,3 +26,5 @@ const auth = getAuth(app);
 const db = getFirestore(app);
 
 export { auth, db };
+
+

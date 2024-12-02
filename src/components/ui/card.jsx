1f18a9ae510c2dfee38779dd20eacbd58caae1e8
khayreali <khayreahmed@gmail.com>
2024-12-01 23:11:01 -0500

    committtt

--- src/components/ui/card.jsx
+++ src/components/ui/card.jsx
@@ -20,13 +20,20 @@ const CardHeader = React.forwardRef(({ className, ...props }, ref) => (
 ));
 CardHeader.displayName = "CardHeader";
 
-const CardTitle = React.forwardRef(({ className, ...props }, ref) => (
-  <h3
-    ref={ref}
-    className={cn("text-2xl font-semibold leading-none tracking-tight", className)}
-    {...props}
-  />
-));
+const CardTitle = React.forwardRef(({ className, children, ...props }, ref) => {
+  if (!children) {
+    return null; // Don't render the heading if there's no content
+  }
+  return (
+    <h3
+      ref={ref}
+      className={cn("text-2xl font-semibold leading-none tracking-tight", className)}
+      {...props}
+    >
+      {children}
+    </h3>
+  );
+});
 CardTitle.displayName = "CardTitle";
 
 const CardDescription = React.forwardRef(({ className, ...props }, ref) => (

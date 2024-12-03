cc236fab8c9c92916a99bc88e6e398a193dd9a04
khayreali <khayreahmed@gmail.com>
2024-12-02 23:45:19 -0500

    Updated routing for Github Pages deployment

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
 import { useAuth } from './contexts/AuthContext';
 
